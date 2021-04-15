d71766765cbd591c4551419357b1b7784c91b46b
Tomas <tkinolli@umich.edu>
2021-04-15 19:41:09 -0400

    create testMixes()

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -7,50 +7,66 @@ public class testSelectionSort {
 	public void test() {
 		testPositive();
 		testNegative();
-		//testMixed();
+		testMixed();
 		//testDuplicates();
-	}    
-	public testSelectionSort() {    
-	}    
-	public void testPositive(){  
+	}
+	public testSelectionSort() {
+	}
+	public void testPositive(){
 		temp = new SelectionSort();
-		int[] arr = new int[5];        
-		arr[0] = 8;        
-		arr[1] = 9;        
-		arr[2] = 7;        
-		arr[3] = 10;        
-		arr[4] = 2;    
-		
-		int[] Sortedarr = new int[5];        
-		Sortedarr[0] = 2;        
-		Sortedarr[1] = 7;        
-		Sortedarr[2] = 8;        
-		Sortedarr[3] = 9;        
-		Sortedarr[4] = 10;   
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
 		arr = temp.basicSelectionSort(arr);
 		assertArrayEquals("Pos Works", Sortedarr, arr);
-		}    
-	public void testNegative(){        
-		int[] arr = new int[5];        
-		arr[0] = -8;        
-		arr[1] = -9;       
-		arr[2] = -7;        
-		arr[3] = -10;       
-		arr[4] = -2;    
-		
-		int[] Sortedarr = new int[5];        
-		Sortedarr[0] = -10;        
-		Sortedarr[1] = -9;        
-		Sortedarr[2] = -8;        
-		Sortedarr[3] = -7;        
-		Sortedarr[4] = -2;   
+		}
+	public void testNegative(){
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = -2;
+
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -9;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -7;
+		Sortedarr[4] = -2;
 		arr = temp.basicSelectionSort(arr);
-		assertArrayEquals("Neg Works", Sortedarr, arr);    
-	}    
+		assertArrayEquals("Neg Works", Sortedarr, arr);
+	}
 	public void testMixed(){
-        /** Test data contains with both positive, negative and zeros **/    
-	}    
-	public void testDuplicates(){        
-		/** Test data contains duplicates **/    
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = 7;
+		arr[3] = -10;
+		arr[4] = 2;
+
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -9;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = 2;
+		Sortedarr[4] = 7;
+		arr = temp.basicSelectionSort(arr);
+		assertArrayEquals("Mix Works", Sortedarr, arr);
+	}
+	public void testDuplicates(){
+		/** Test data contains duplicates **/
 	}
 }
