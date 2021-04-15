43ea19b821f1d62dd147b2d9f095e9f29a8e41dd
Tomas <tkinolli@umich.edu>
2021-04-15 19:33:09 -0400

    upload files to repos

---
+++ testSelectionSort.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+public class testSelectionSort {
+	private SelectionSort temp;
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		//testMixed();
+		//testDuplicates();
+	}    
+	public testSelectionSort() {    
+	}    
+	public void testPositive(){  
+		temp = new SelectionSort();
+		int[] arr = new int[5];        
+		arr[0] = 8;        
+		arr[1] = 9;        
+		arr[2] = 7;        
+		arr[3] = 10;        
+		arr[4] = 2;    
+		
+		int[] Sortedarr = new int[5];        
+		Sortedarr[0] = 2;        
+		Sortedarr[1] = 7;        
+		Sortedarr[2] = 8;        
+		Sortedarr[3] = 9;        
+		Sortedarr[4] = 10;   
+		arr = temp.basicSelectionSort(arr);
+		assertArrayEquals("Pos Works", Sortedarr, arr);
+		}    
+	public void testNegative(){        
+		int[] arr = new int[5];        
+		arr[0] = -8;        
+		arr[1] = -9;       
+		arr[2] = -7;        
+		arr[3] = -10;       
+		arr[4] = -2;    
+		
+		int[] Sortedarr = new int[5];        
+		Sortedarr[0] = -10;        
+		Sortedarr[1] = -9;        
+		Sortedarr[2] = -8;        
+		Sortedarr[3] = -7;        
+		Sortedarr[4] = -2;   
+		arr = temp.basicSelectionSort(arr);
+		assertArrayEquals("Neg Works", Sortedarr, arr);    
+	}    
+	public void testMixed(){
+        /** Test data contains with both positive, negative and zeros **/    
+	}    
+	public void testDuplicates(){        
+		/** Test data contains duplicates **/    
+	}
+}
